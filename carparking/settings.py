0f4f2ff49c4724d0a7dfda2e530c7bad7fda40f4
Deepak <deepak.bandawane@fundspi.com>
2023-07-20 11:47:13 +0530

    debug

--- carparking/settings.py
+++ carparking/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-2__c#$qy3xrzb6_kohtp$8rainfwc-n+j206b+z6n8gi3z0o(=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
